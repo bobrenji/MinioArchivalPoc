cd209ee5310d6daae4dcd37d5b928ebec81fb60a
Renji <renji129@gmail.com>
2024-05-04 22:14:41 +0800

    Create an empty project with minio as dependency

---
+++ src/main/java/com/demo/miniofilereader/MinioFileReaderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.miniofilereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioFileReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioFileReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/miniofilereader/MinioFileReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.miniofilereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioFileReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
